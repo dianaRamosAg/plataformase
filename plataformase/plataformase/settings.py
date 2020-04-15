378b88644c573ee9a7e8ed9c5eed40fe833277e4
dianaRamosAg <dilaramosag@ittepic.edu.mx>
2020-04-15 10:25:45 -0600

    settings

--- plataformase/plataformase/settings.py
+++ plataformase/plataformase/settings.py
@@ -41,9 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR,"static"),
-)
+# STATICFILES_DIRS = (
+#   os.path.join(BASE_DIR,"static"),
+# )
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,3 +135,7 @@ LOGIN_REDIRECT_URL = 'perfil'
 LOGOUT_REDIRECT_URL = 'login'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- plataformase/plataformase/urls.py
+++ plataformase/plataformase/urls.py
@@ -40,5 +40,8 @@ urlpatterns = [
 
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += [] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [] + static(settings.MEDIA_URL)
+
