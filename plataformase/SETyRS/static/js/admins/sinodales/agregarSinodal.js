34cce8cc548b60e3c33d48953e67b20cdf85ad93
SalmeronU <ulalegriasa@ittepic.edu.mx>
2020-08-12 17:12:22 -0600

    botones de crear solicitudes desactivados terminado

---
+++ plataformase/SETyRS/static/js/admins/sinodales/agregarSinodal.js
@@ -0,0 +1,31 @@
+
+function cargarListeners() { 
+    var btnC = document.getElementById("btnContinuar");
+    btnC.addEventListener("click",continuarSoli,false);
+};
+
+function continuarSoli(){
+    var divContainer = document.getElementById("buttonContainer");
+
+    divContainer.innerHTML ="";
+    //creamos el boton de cargando...
+    var creandoBoton = document.createElement("button");
+    creandoBoton.className="btn btn-primary mt-3";
+    creandoBoton.innerHTML="Creando solicitud..."
+    creandoBoton.disabled=true;
+
+    //creamos el span para que gire
+    var spanBoton = document.createElement("span");
+    spanBoton.className="spinner-border spinner-border-sm";
+
+    //agregamos el span al boton y el boton al div contenedor
+    creandoBoton.appendChild(spanBoton);
+    divContainer.appendChild(creandoBoton);
+
+    //Depués de haber agregado el botón, enviamos el form para crear la solicitud
+
+    setTimeout(function(){ document.getElementById("agregarSinodales").submit(); }, 2000);
+    
+};
+
+  document.addEventListener("DOMContentLoaded", cargarListeners, false);
