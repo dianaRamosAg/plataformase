190f39c1a1829e1a0b84c9fbb0373d76754e4711
ZulmaCoronel <31231057+ZulmaCoronel@users.noreply.github.com>
2020-11-12 01:24:57 -0700

    Select que muestra las instituciones para programar supervision

--- plataformase/SigApp/views.py
+++ plataformase/SigApp/views.py
@@ -22,16 +22,6 @@ def inicio(request):
 def nuevaBase(request):
    return render (request, "SigApp/nuevaBase.html")
 
-
-
-
-
-
-
-
-
-
-
 def actas_departamento(request):
     return render (request, "SigApp/actas_departamento.html")
 
@@ -42,7 +32,58 @@ def historial_institucion(request):
     return render (request, "SigApp/historial_institucion.html")
 
 def programar_superv(request):
-    return render (request, "SigApp/programar_superv.html")
+    GradosAcademicos = GradoAcademico.objects.all()
+    instituciones = EscuelaC.objects.all()
+
+    Localidades = Localidad.objects.all()
+
+    AreasIntereses = AreaInteres.objects.all()
+    Municipios = Municipio.objects.all()
+
+    #-----------------------------------------------------------------------
+    usuario = request.user.id; #OBTENER NUMERO DE INSTITUCIONES
+    numInst = UsuarioInstitucion.objects.filter(id_usuariobase_id=usuario)
+    nI = 0
+    for i in numInst:
+        nI += 1
+    #-----------------------------------------------------------------------
+    if request.user.is_authenticated:
+        if request.user.departamento_id:
+
+            return render(request,'SigApp/programar_superv.html',{
+                "opcionesinstituciones": instituciones,
+                "opcionesgrados": GradosAcademicos,
+                "areaseducacion":AreasIntereses,
+                "opcionesmunicipios": Municipios,
+                "localidades": Localidades,
+                "numeroInstituciones": nI,
+                "numeroModificaciones": notificaiones(request.user.departamento_id),
+            })
+        else:
+            return render(request,'SigApp/programar_superv.html',{
+                "opcionesinstituciones": instituciones,
+                "opcionesgrados": GradosAcademicos,
+                "areaseducacion":AreasIntereses,
+                "opcionesmunicipios": Municipios,
+                "localidades": Localidades,
+                "numeroInstituciones": nI,
+            })
+    else: 
+        GradosAcademicos = GradoAcademico.objects.all()
+        instituciones = EscuelaC.objects.all()
+
+        Localidades = Localidad.objects.all()
+
+        AreasIntereses = AreaInteres.objects.all()
+        Municipios = Municipio.objects.all()
+        return render(request,'SigApp/programar_superv.html',{
+            "opcionesinstituciones": instituciones,
+            "opcionesgrados": GradosAcademicos,
+            "areaseducacion":AreasIntereses,
+            "opcionesmunicipios": Municipios,
+            "localidades": Localidades,
+        })
+
 
 def infosistemas(request):
     return render (request,"SigApp/infosistemas.html")
@@ -1963,7 +2004,6 @@ def solicitaModEstadistica(request, clave, claveE, id_dep):
 def solicitaModeEstadistica(request, clave,claveE, id_dep):
     
     carrera = RVOES.objects.get(ClaveCarrera = clave, ClaveEscuela_id = claveE)
-
      
     if request.method == 'POST':
             
@@ -1978,27 +2018,20 @@ def solicitaModeEstadistica(request, clave,claveE, id_dep):
         tipo = request.POST['tipo']
         periodo = request.POST['periodo']
         modalidad = request.POST['modalidad']
-
         carrera.TotalPrimero = TotalPri
         carrera.TotalSegundo = TotalSeg
         carrera.TotalTercero = TotalTer
         carrera.TotalCuarto = TotalCua
         carrera.TotalQuinto = TotalQui
         carrera.TotalSexto = TotalSexto
-
         
         carrera.save()
-
-
-
         Escuela = EscuelaC.objects.get(ClaveEscuela = claveE)
         escuelaEstadistica = estadisticosNuevo.objects.get(ClaveEscuela=claveE)
-
         try:
             escuelaEstadistica = estadisticosNuevo.objects.get(ClaveEscuela=claveE)
         except estadisticosNuevo.DoesNotExist:
             escuelaEstadistica = None
-
         try:
             rvoes = RVOES.objects.filter(ClaveEscuela=claveE)
         except RVOES.DoesNotExist:
@@ -2011,15 +2044,12 @@ def solicitaModeEstadistica(request, clave,claveE, id_dep):
             "rvoes": rvoes,
             "carrera": carrera,
         })
-
     Escuela = EscuelaC.objects.get(ClaveEscuela = claveE)
     escuelaEstadistica = estadisticosNuevo.objects.get(ClaveEscuela=claveE)
-
     try:
         escuelaEstadistica = estadisticosNuevo.objects.get(ClaveEscuela=claveE)
     except estadisticosNuevo.DoesNotExist:
         escuelaEstadistica = None
-
     try:
         rvoes = RVOES.objects.filter(ClaveEscuela=claveE)
     except RVOES.DoesNotExist:
@@ -2031,11 +2061,8 @@ def solicitaModeEstadistica(request, clave,claveE, id_dep):
          "id_dep":id_dep,  
          "rvoes": rvoes,
          "carrera": carrera,
-
-
     }) '''
  
 
 
-    
     
